d9170302df2ad0b7e71d1b9c8cbd4f95aaec52fd
admin <harshmhar@gmail.com>
2024-08-27 08:36:18 +0530

    All Java programs files added

--- src/ex_07072024/Lob006.java
+++ src/ex_07072024/Lob006.java
@@ -2,6 +2,10 @@ package ex_07072024;
 
 public class Lob006 {
     public static void main(String[] args) {
-        System.out.println("Commit and push the code");
+        System.out.println("Commit and push the code1");
+        System.out.println("Commit and push the code2");
+        System.out.println("Commit and push the code3");
+
+
     }
 }
