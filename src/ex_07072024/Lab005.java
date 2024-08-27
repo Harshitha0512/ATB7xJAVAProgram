a0abddafcff0646e0593f39242f5f0b39524e576
admin <harshmhar@gmail.com>
2024-08-27 08:41:59 +0530

    All Java programs files added

--- src/ex_07072024/Lab005.java
+++ src/ex_07072024/Lab005.java
@@ -1,4 +1,11 @@
 package ex_07072024;
 
 public class Lab005{
+    public static void main(String[] args) { // main method - Execution starts from here.
+        System.out.println("Hello");//print with new line
+        System.out.print("Hi");//print in same line
+        System.out.print("Hi");//print in same line
+        System.out.print("Hi");//print in same line
+
+    }
 }
